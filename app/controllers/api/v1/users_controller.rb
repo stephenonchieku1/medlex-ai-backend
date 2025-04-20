38e8335a9f92cf94123db2c00437d219928e2594
stephenonchieku1 <onchiekustephen901@gmail.com>
2025-04-20 21:58:26 +0300

    Implement user creation with strong parameters and error handling in UsersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,41 @@
+# module Api
+#   module V1
+#     class UsersController < BaseController
+#       skip_before_action :authenticate_user, only: [:create]
+
+#       def create
+#         # Extract email and password from params
+#         email = params[:email] || params.dig(:user, :email)
+#         password = params[:password] || params.dig(:user, :password)
+#         password_confirmation = params[:password_confirmation] || params.dig(:user, :password_confirmation)
+
+#         @user = User.new(
+#           email: email,
+#           password: password,
+#           password_confirmation: password_confirmation
+#         )
+        
+#         if @user.save
+#           render json: {
+#             token: @user.auth_token,
+#             user: {
+#               id: @user.id,
+#               email: @user.email
+#             }
+#           }, status: :created
+#         else
+#           render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+#         end
+#       end
+
+#       private
+
+#       def user_params
+#         params.permit(:email, :password, :password_confirmation)
+#       end
+#     end
+#   end
+# end
 module Api
   module V1
     class UsersController < BaseController
@@ -5,6 +43,7 @@ module Api
 
       def create
         @user = User.new(user_params)
+        
         if @user.save
           render json: {
             token: @user.auth_token,
@@ -25,4 +64,4 @@ module Api
       end
     end
   end
-end 
+end
